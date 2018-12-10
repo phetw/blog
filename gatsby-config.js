a996484a42670d8b43164f2737964055ed276a91
was.li <wasuwat.se@gmail.com>
2018-12-11 00:16:29 +0700

    remove unused source path

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,13 +63,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         head: true,
