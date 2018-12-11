55223f1d9aa77e1934da1bcaef967c330682b904
was.li <wasuwat.se@gmail.com>
2018-12-11 19:01:33 +0700

    minor code format

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,6 @@ module.exports = {
   pathPrefix: '/blog',
   plugins: [
     {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/img`,
