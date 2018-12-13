75767584fa2b581573ce705bc8044fdbe56e5404
was.li <wasuwat.se@gmail.com>
2018-12-13 20:02:14 +0700

    add gatsby-plugin-sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,6 +105,7 @@ module.exports = {
         cacheId: `wasuwat-blog`,
       },
     },
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-netlify',
