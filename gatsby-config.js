13698df4b3f9973b6c880d8f3f422e5f6d051c8a
was.li <wasuwat.se@gmail.com>
2018-12-13 16:40:48 +0700

    remove netlify identity plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,7 +106,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-styled-components',
-    'gatsby-plugin-netlify-identity-widget',
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-netlify',
   ],
