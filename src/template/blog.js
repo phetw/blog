9f7efa487249efec2eab019223bfa4d28da9ad0d
was.li <wasuwat.se@gmail.com>
2018-12-14 17:42:16 +0700

    migrate to styled components

--- src/template/blog.js
+++ src/template/blog.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 import { graphql, Link } from 'gatsby'
 
+const Navbar = styled.nav`
+  display: flex;
+  flex-direction: row;
+`
+
+const BlogContainer = styled.main`
+  max-width: 800px;
+  margin: 0 auto;
+`
+
+const ContentWrapper = styled.section`
+  margin: 1.25rem 1.75rem;
+`
+
+const Title = styled.h1`
+  margin: 1.35rem 0;
+`
+
+const PublishDate = styled.p`
+  color: #c3c3c3;
+  margin-bottom: 1rem;
+  font-size: 75%;
+`
+
+const ShareToSocial = styled.section`
+  float: right;
+  margin-bottom: 2rem;
+`
+
 const BlogTemplate = ({ data: { site, markdownRemark: post } }) => (
-  <section style={{ maxWidth: '800px', margin: '0 auto' }}>
+  <BlogContainer>
     <Helmet
       htmlAttributes={{ lang: 'th' }}
       title={`${post.frontmatter.title} - ${site.siteMetadata.title}`}
@@ -26,19 +56,32 @@ const BlogTemplate = ({ data: { site, markdownRemark: post } }) => (
         },
       ]}
     />
-    <div style={{ margin: '1.25rem 1.75rem' }}>
-      <Link style={{ fontSize: '1rem' }} to="/">
-        Home
-      </Link>
-      <h1 style={{ margin: '1.35rem 0' }}>{post.frontmatter.title}</h1>
-      <p style={{ opacity: '0.5', marginBottom: '1rem', fontSize: '75%' }}>
-        {post.frontmatter.date}
-      </p>
+    <ContentWrapper>
+      <Navbar>
+        <Link style={{ fontSize: '1rem' }} to="/">
+          Home
+        </Link>
+      </Navbar>
+      <Title>{post.frontmatter.title}</Title>
+      <PublishDate>{post.frontmatter.date}</PublishDate>
       <hr />
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <section dangerouslySetInnerHTML={{ __html: post.html }} />
       <hr />
-    </div>
-  </section>
+      <ShareToSocial>
+        Share to :{' '}
+        <a
+          href={`https://facebook.com/sharer.php?u=${
+            site.siteMetadata.siteUrl
+          }${post.frontmatter.path}`}
+          rel="nofollow"
+          target="_blank"
+          title="Share on Facebook"
+        >
+          Facebook
+        </a>
+      </ShareToSocial>
+    </ContentWrapper>
+  </BlogContainer>
 )
 
 export default BlogTemplate
@@ -50,11 +93,12 @@ export const pageQuery = graphql`
         title
         description
         author
+        siteUrl
       }
     }
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
-      excerpt(format: PLAIN)
+      excerpt(format: PLAIN, pruneLength: 300)
       frontmatter {
         path
         title
