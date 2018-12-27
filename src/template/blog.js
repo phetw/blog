1d358a0b9a3dcbda6dab36e685372b9d5a5c305c
was.li <wasuwat.se@gmail.com>
2018-12-27 13:42:43 +0700

    fix blog template

--- src/template/blog.js
+++ src/template/blog.js
@@ -1,70 +1,122 @@
-import React, { memo } from 'react'
+import React, { Fragment, memo } from 'react'
+import Helmet from 'react-helmet'
 import styled from 'styled-components'
+import { graphql, Link } from 'gatsby'
 
-import ProfileImageSrc from '../../static/img/me.jpg'
+import ShareToSocial from '../components/ShareToSocial'
 
-const SectionWrapper = styled.section`
+const Navbar = styled.nav`
   display: flex;
   flex-direction: row;
-  align-content: center;
-  margin-top: 1.5rem;
-  @media screen and (min-width: 700px) {
-    height: 120px;
-  }
-
-  @media screen and (max-width: 699px) {
-    height: 90px;
-  }
 `
 
-const ProfileImage = styled.img`
-  align-self: center;
-  @media screen and (min-width: 700px) {
-    width: 105px;
-    height: 100px;
-    border-radius: 50%;
-    margin-bottom: 0;
-    margin-right: 1.5rem;
-  }
-
-  @media screen and (max-width: 699px) {
-    display: none;
-  }
+const BlogContainer = styled.main`
+  max-width: 800px;
+  margin: 0 auto;
 `
 
-const ProfileDetails = styled.section`
-  align-self: center;
+const ContentWrapper = styled.section`
+  margin: 1.25rem 1.75rem;
 `
 
-const ProfileName = styled.a`
-  color: #252525;
-  margin: 0;
-  font-size: 120%;
-  font-weight: 700;
+const Title = styled.h1`
+  margin: 1.35rem 0;
 `
 
-const CoolOneLiner = styled.p`
-  color: #bbbbbb;
-  font-size: 80%;
-  margin: 0;
+const PublishDate = styled.p`
+  color: #c3c3c3;
+  margin-bottom: 1rem;
+  font-size: 75%;
 `
 
-const WhoAMI = memo(() => (
-  <SectionWrapper>
-    <ProfileImage src={ProfileImageSrc} />
-    <ProfileDetails>
-      <ProfileName
-        href="https://rappad.github.io/wasuwat-limsuparhat"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Wasuwat Limsuparhat
-      </ProfileName>
-      <CoolOneLiner>
-        I write to remind and keep track on what I've learn.
-      </CoolOneLiner>
-    </ProfileDetails>
-  </SectionWrapper>
+const BlogTemplate = memo(({ data: { site, markdownRemark: post } }) => (
+  <Fragment>
+    <Helmet
+      htmlAttributes={{ lang: 'th' }}
+      title={`${post.frontmatter.title} - ${site.siteMetadata.title}`}
+      meta={[
+        { name: 'author', content: site.siteMetadata.author },
+        {
+          name: 'description',
+          content: `${post.frontmatter.description} | ${
+            post.excerpt
+          } written by ${site.siteMetadata.author}`,
+        },
+        { name: 'og:title', content: post.frontmatter.title },
+        {
+          name: 'og:description',
+          content: `${post.frontmatter.description} written by ${
+            site.siteMetadata.author
+          }`,
+        },
+        {
+          name: 'og:image',
+          content: post.frontmatter.thumbnail.childImageSharp.resize.src,
+        },
+        {
+          name: 'twitter:title',
+          content: post.frontmatter.title,
+        },
+        {
+          name: 'twitter:description',
+          content: post.frontmatter.description,
+        },
+        {
+          name: 'twitter:image',
+          content: post.frontmatter.thumbnail.childImageSharp.resize.src,
+        },
+      ]}
+    />
+    <BlogContainer>
+      <ContentWrapper>
+        <Navbar>
+          <Link style={{ fontSize: '1rem' }} to="/">
+            Home
+          </Link>
+        </Navbar>
+        <Title>{post.frontmatter.title}</Title>
+        <PublishDate>{post.frontmatter.date}</PublishDate>
+        <hr />
+        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        <hr />
+        <ShareToSocial
+          title={post.frontmatter.title}
+          path={post.frontmatter.path}
+          siteUrl={site.siteMetadata.siteUrl}
+        />
+      </ContentWrapper>
+    </BlogContainer>
+  </Fragment>
 ))
 
-export default WhoAMI
+export default BlogTemplate
+
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        siteUrl
+      }
+    }
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      excerpt(format: PLAIN, pruneLength: 300)
+      frontmatter {
+        path
+        title
+        description
+        date(formatString: "DD MMMM, YYYY")
+        thumbnail {
+          childImageSharp {
+            resize(width: 800, height: 420, cropFocus: CENTER, toFormat: JPG) {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
