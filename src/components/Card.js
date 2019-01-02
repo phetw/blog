63b5e2a921bcb919eed9c1437776600e10c1e179
was.li <wasuwat.se@gmail.com>
2019-01-02 17:16:51 +0700

    add Card Component

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import React, { memo } from 'react'
+import styled from 'styled-components'
+
+const StyledCard = styled.section`
+  background-color: white;
+  box-shadow: 0 30px 75px 0 rgba(10, 25, 41, 0.2);
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 560px;
+  width: 100%;
+  @media (max-width: 600px) {
+    padding: 1.75rem;
+  }
+  @media (min-width: 601px) {
+    padding: 2rem 3.5rem;
+  }
+`
+
+const Card = memo(({ children }) => <StyledCard>{children}</StyledCard>)
+
+export default Card
