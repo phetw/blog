0487274f47f9b0ba35945a0b06ff07f8d9ef7c6a
rappad <wasuwat.se@gmail.com>
2019-06-02 12:39:57 +0700

    remove log

--- src/components/blog/Footer.js
+++ src/components/blog/Footer.js
@@ -15,7 +15,6 @@ const ShareToSocialWrapper = styled.section`
 
 const ShareToSocial = memo(({ title = '', path, siteUrl }) => (
   <ShareToSocialWrapper>
-    {console.log(siteUrl, path)}
     Share to :{' '}
     <a
       href={`https://facebook.com/sharer.php?u=${siteUrl}${path}`}
