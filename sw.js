e24c7d9e1139dd513f098ec9ce8c72e015002dc3
Wasuwat Lmimsuparhat <wasuwat.se@gmail.com>
2019-06-02 03:17:34 +0000

    autodeploy [ci skip]

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d645dacfa74d00934122.js"
+    "url": "webpack-runtime-9e52987db9fe3d05e700.js"
   },
   {
     "url": "styles.e34bead5c6f22ca576c8.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7362534cab1329092d2e3296615138f7"
+    "revision": "35f891762555ef0f8b5a656c32dfe03c"
   },
   {
     "url": "component---src-pages-404-js-16b2470fdc93a1060453.js"
